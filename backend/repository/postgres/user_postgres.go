831fa7c76957d9fec7128e0c17a75790e1ae977c
Abraham14711 <pet_zed@rambler.ru>
2025-07-11 07:29:17 -0700

    feat: add all the models + postgres connections related to these models

--- backend/repository/postgres/user_postgres.go
+++ backend/repository/postgres/user_postgres.go
@@ -28,6 +28,14 @@ func (r *UserRepository) GetUserByID(id uuid.UUID) (*model.User, error) {
 	return &user, nil
 }
 
+func (r *UserRepository) GetGoalsByUserID(id uuid.UUID) ([]model.Goal, error) {
+	var goals []model.Goal
+	if err := r.db.Where("user_id = ?", id).Find(&goals).Error; err != nil {
+		return nil, err
+	}
+	return goals, nil
+}
+
 func (r *UserRepository) UpdateUser(user *model.User) error {
 	return r.db.Save(user).Error
 }
