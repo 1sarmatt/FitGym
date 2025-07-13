b00cddb03b29ed0fbe64e3430c6b8c6c6cb209f0
Abraham14711 <pet_zed@rambler.ru>
2025-07-13 22:01:57 +0300

    fix: add main go prev

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,61 +1,54 @@
 package main
 
 import (
-	"fitgym/backend/handlers"
-	"fitgym/backend/internal"
-	"fitgym/backend/repository/model"
-	"fitgym/backend/serivces"
-	"log"
-	"net/http"
-	"os"
+  "fitgym/backend/handlers"
+  "fitgym/backend/internal"
+  "fitgym/backend/repository/model"
+  "fitgym/backend/serivces"
+  "log"
+  "net/http"
+  "os"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+  "github.com/go-chi/chi/v5"
+  "github.com/joho/godotenv"
+  "gorm.io/driver/postgres"
+  "gorm.io/gorm"
 )
 
 func main() {
-	// Load environment variables
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found, using environment variables")
-	}
+  // Load environment variables
+  if err := godotenv.Load(); err != nil {
+    log.Println("No .env file found, using environment variables")
+  }
 
-	db, err := gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err)
-	}
-	db.AutoMigrate(&model.User{})
+  db, err := gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{})
+  if err != nil {
+    log.Fatal(err)
+  }
+  db.AutoMigrate(&model.User{})
 
-	r := chi.NewRouter()
+  r := chi.NewRouter()
 
-	workoutService := serivces.NewWorkoutService()
-	handlers.WorkoutService = workoutService
+  workoutService := serivces.NewWorkoutService()
+  handlers.WorkoutService = workoutService
 
-	r.Get("/login", func(w http.ResponseWriter, r *http.Request) {
-		token, err := internal.GenerateJWT("123")
-		if err != nil {
-			http.Error(w, "Could not generate token", http.StatusInternalServerError)
-			return
-		}
-		w.Write([]byte(token))
-	})
+  r.Post("/addWorkout", handlers.AddWorkoutHandler)
+  r.Post("/addExercise", handlers.AddExerciseHandler)
+  r.Get("/GetWorkoutHistory", handlers.GetWorkoutHistoryHandler)
+  r.Post("/AddFriend", handlers.AddFriendHandler)
+  r.Get("/GetFriends", handlers.GetFriendsHandler)
+  r.Post("/Register", handlers.RegisterUserHandler)
+  r.Get("/Login", handlers.LoginUserHandler)
 
-	r.Post("/addWorkout", handlers.AddWorkoutHandler)
-	r.Post("/addExercise", handlers.AddExerciseHandler)
-	r.Get("/GetWorkoutHistory", handlers.GetWorkoutHistoryHandler)
-	r.Post("/AddFriend", handlers.AddFriendHandler)
-	r.Get("/GetFriends", handlers.GetFriendsHandler)
+  // Protected route
+  r.With(internal.JWTAuthMiddleware).Get("/protected", func(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("You are authenticated!"))
+  })
 
-	// Protected route
-	r.With(internal.JWTAuthMiddleware).Get("/protected", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("You are authenticated!"))
-	})
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	log.Printf("Server running on :%s", port)
-	log.Fatal(http.ListenAndServe(":"+port, r))
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = "8080"
+  }
+  log.Printf("Server running on :%s", port)
+  log.Fatal(http.ListenAndServe(":"+port, r))
 }
