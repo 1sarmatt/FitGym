55d694c946fe36c3e613b8f9eb7ee0087dd52e64
Abraham14711 <pet_zed@rambler.ru>
2025-07-13 22:12:54 +0300

    fix: some fixes related to handlers usage

--- backend/handlers/friend_handler.go
+++ backend/handlers/friend_handler.go
@@ -10,6 +10,8 @@ import (
 	"github.com/google/uuid"
 )
 
+var FriendRepo *postgres.FriendRepository
+
 func AddFriendHandler(w http.ResponseWriter, r *http.Request) {
 	type reqBody struct {
 		UserID   uuid.UUID `json:"user_id"`
@@ -17,8 +19,10 @@ func AddFriendHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	var req reqBody
 
-	err := postgres.AddFriend(req.UserID, req.FriendID)
-	log.Logger(err)
+	err := friendRepo.AddFriend(req.UserID, req.FriendID)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func GetFriendsHandler(w http.ResponseWriter, r *http.Request) {
@@ -26,7 +30,9 @@ func GetFriendsHandler(w http.ResponseWriter, r *http.Request) {
 		UserID uuid.UUID `json:"user_id"`
 	}
 	var req reqBody
-	user, err := postgres.GetFriends(req.UserID)
-	log.Logger(err)
+	user, err := friendRepo.GetFriends(req.UserID)
+	if err != nil {
+		log.Fatal(err)
+	}
 	json.NewEncoder(w).Encode(user)
 }
--- backend/handlers/user_handler.go
+++ backend/handlers/user_handler.go
@@ -1,72 +1,72 @@
 package handlers
 
 import (
-  "encoding/json"
-  "net/http"
+	"encoding/json"
+	"net/http"
 
-  "fitgym/backend/repository/postgres"
-  "fitgym/backend/serivces"
+	"fitgym/backend/repository/postgres"
+	"fitgym/backend/serivces"
 )
 
 var userHandler *serivces.User_Service
-var userRepo *postgres.UserRepository // Should be initialized in main.go
+var UserRepo *postgres.UserRepository // Should be initialized in main.go
 
 // Handler for editing user profile
 func EditProfileHandler(w http.ResponseWriter, r *http.Request) {
-  type reqBody struct {
-    Name string `json:"name"`
-    Age  int    `json:"age"`
-  }
-  var req reqBody
-  if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-    http.Error(w, "Invalid request body", http.StatusBadRequest)
-    return
-  }
-  ok, err := userHandler.EditProfile(req.Name, req.Age, *userRepo)
-  if err != nil || !ok {
-    http.Error(w, err.Error(), http.StatusBadRequest)
-    return
-  }
-  w.WriteHeader(http.StatusOK)
-  json.NewEncoder(w).Encode(map[string]string{"status": "profile updated"})
+	type reqBody struct {
+		Name string `json:"name"`
+		Age  int    `json:"age"`
+	}
+	var req reqBody
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+	ok, err := userHandler.EditProfile(req.Name, req.Age, *userRepo)
+	if err != nil || !ok {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"status": "profile updated"})
 }
 
 // Handler for registration of user
 func RegisterUserHandler(w http.ResponseWriter, r *http.Request) {
-  const errInvalidRequestBody = "Invalid request body"
-  type reqBody struct {
-    Email    string `json:"email"`
-    Password string `json:"password"`
-  }
-  var req reqBody
-  if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-    http.Error(w, errInvalidRequestBody, http.StatusBadRequest)
-    return
-  }
-  token, err := userHandler.Register(req.Email, req.Password, *userRepo)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusBadRequest)
-    return
-  }
-  json.NewEncoder(w).Encode(map[string]string{"token": token})
+	const errInvalidRequestBody = "Invalid request body"
+	type reqBody struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	var req reqBody
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, errInvalidRequestBody, http.StatusBadRequest)
+		return
+	}
+	token, err := userHandler.Register(req.Email, req.Password, *userRepo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	json.NewEncoder(w).Encode(map[string]string{"token": token})
 }
 
 // Handler for login of user
 func LoginUserHandler(w http.ResponseWriter, r *http.Request) {
-  const errInvalidRequestBody = "Invalid request body"
-  type reqBody struct {
-    Email    string `json:"email"`
-    Password string `json:"password"`
-  }
-  var req reqBody
-  if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-    http.Error(w, errInvalidRequestBody, http.StatusBadRequest)
-    return
-  }
-  token, err := userHandler.Login(req.Email, req.Password, *userRepo)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusUnauthorized)
-    return
-  }
-  json.NewEncoder(w).Encode(map[string]string{"token": token})
+	const errInvalidRequestBody = "Invalid request body"
+	type reqBody struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	var req reqBody
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, errInvalidRequestBody, http.StatusBadRequest)
+		return
+	}
+	token, err := userHandler.Login(req.Email, req.Password, *userRepo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+	json.NewEncoder(w).Encode(map[string]string{"token": token})
 }
