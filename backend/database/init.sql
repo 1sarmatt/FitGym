e1c345eae4bfca75faa9e0059c6c97beb580bab9
ph1larmon1a <a.fominykh@innopolis.university>
2025-07-13 21:34:01 +0300

    feat(devops): Dockerize frontend

--- backend/database/init.sql
+++ backend/database/init.sql
@@ -13,7 +13,7 @@ CREATE TABLE Goal (
     user_id UUID PRIMARY KEY,
     weekly_sessions INT NOT NULL,
     current_progress INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE CASCADE
 );
 
 -- Friend table (many-to-many self-referencing)
@@ -21,21 +21,18 @@ CREATE TABLE Friend (
     user_id UUID NOT NULL,
     friend_id UUID NOT NULL,
     PRIMARY KEY (user_id, friend_id),
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (friend_id) REFERENCES users(id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE CASCADE,
+    FOREIGN KEY (friend_id) REFERENCES User(id) ON DELETE CASCADE
 );
 
--- Workout table
-CREATE TYPE workout_type AS ENUM ('cardio', 'strength', 'mobility', 'other');  -- example types
-
 CREATE TABLE Workout (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id INT PRIMARY KEy,
     user_id UUID NOT NULL,
-    type workout_type NOT NULL,
+    type VARCHAR(100) NOT NULL,
     date DATE NOT NULL,
     duration INT NOT NULL,
     notes TEXT,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE CASCADE
 );
 
 -- Exercise table
@@ -46,5 +43,5 @@ CREATE TABLE Exercise (
     sets INT NOT NULL,
     reps INT NOT NULL,
     weight FLOAT NOT NULL,
-    FOREIGN KEY (workout_id) REFERENCES workouts(id) ON DELETE CASCADE
+    FOREIGN KEY (workout_id) REFERENCES Workout(id) ON DELETE CASCADE
 );
