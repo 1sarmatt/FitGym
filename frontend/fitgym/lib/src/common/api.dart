7f734328b527cd9dd1b01bc46cc29b95b19c0b5e
Abraham14711 <pet_zed@rambler.ru>
2025-07-16 18:25:15 +0300

    fix: temporal hardcode

--- frontend/fitgym/lib/src/common/api.dart
+++ frontend/fitgym/lib/src/common/api.dart
@@ -1,10 +1,9 @@
 import 'dart:convert';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
-  static String baseUrl = String.fromEnvironment('API_URL');
+  static const String baseUrl = 'http://193.148.58.102'; 
 
   // Registration
   static Future<http.Response> register(String email, String password) {
