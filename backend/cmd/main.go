3f19fdc31c0749fcaceffde15c9c1580c4c5953a
Abraham14711 <pet_zed@rambler.ru>
2025-07-16 17:55:10 +0300

    fix: cors

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -18,7 +18,7 @@ import (
 
 func withCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "https://1sarmatt.github.io/")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		if r.Method == "OPTIONS" {
